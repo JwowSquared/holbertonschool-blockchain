6c74d6645221810aae80d78c11e731cf5767c517
JwowSquared <Jdwagner1@hotmail.com>
2022-02-23 10:32:48 +0000

    fixed betty

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -8,7 +8,8 @@
 *
 * Return: address of hash
 */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+	uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (digest == NULL)
 		return (NULL);
