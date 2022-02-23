6e789fb19698e6703bee714b0de0b53d305b8818
JwowSquared <Jdwagner1@hotmail.com>
2022-02-23 10:31:20 +0000

    setup for task 0 completed, added task 0 source code

---
+++ crypto/sha256.c
@@ -0,0 +1,17 @@
+#include "hblk_crypto.h"
+
+/**
+* sha256 - hashes s and stores in digest
+* @s: sequence of bytes to hash
+* @len: length of s
+* @digest: memory location to store hash
+*
+* Return: address of hash
+*/
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+	if (digest == NULL)
+		return (NULL);
+
+	return (SHA256((unsigned char *)s, len, digest));
+}
