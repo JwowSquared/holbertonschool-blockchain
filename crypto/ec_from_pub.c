d72f2630b918d68d64151b1569203bbb7a22b0c0
JwowSquared <Jdwagner1@hotmail.com>
2022-03-03 03:41:41 +0000

    now handles when pub is NULL

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -13,6 +13,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	const EC_GROUP *group = NULL;
 	BN_CTX *ctx;
 
+	if (pub == NULL)
+		return (NULL);
+
 	ctx = BN_CTX_new();
 
 	out = EC_KEY_new_by_curve_name(EC_CURVE);
