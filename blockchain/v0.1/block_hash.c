4831637afa8fb0bcf42bdfa66a8070635441190d
JwowSquared <Jdwagner1@hotmail.com>
2022-03-26 16:41:51 +0000

    hashable size is now all of info but only the occupied portion of data.buffer

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -10,7 +10,7 @@
 uint8_t *block_hash(block_t const *block,
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t hashable_size = sizeof(block->data) + sizeof(block->info);
+	size_t hashable_size = sizeof(block->info) + block->data.len;
 
 	return (sha256((int8_t const *)block, hashable_size, hash_buf));
 }
