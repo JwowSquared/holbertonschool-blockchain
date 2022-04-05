7975f07bfdc237dd2994d602b142d51575e7097c
JwowSquared <Jdwagner1@hotmail.com>
2022-04-05 03:34:40 +0000

    added task1 source code

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -12,9 +12,9 @@
  * struct transaction_s - Transaction structure
  *
  * @id: Transaction ID. A hash of all the inputs and outputs.
- * 	Prevents further alteration of the transaction.
- * @inputs:  	List of `tx_in_t *`. Transaction inputs
- * @outputs: 	List of `tx_out_t *`. Transaction outputs
+ *	Prevents further alteration of the transaction.
+ * @inputs: List of `tx_in_t *`. Transaction inputs
+ * @outputs: List of `tx_out_t *`. Transaction outputs
  */
 typedef struct transaction_s
 {
@@ -79,6 +79,11 @@ typedef struct unspent_tx_out_s
 
 
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+
+unspent_tx_out_t *unspent_tx_out_create(
+uint8_t block_hash[SHA256_DIGEST_LENGTH],
+uint8_t tx_id[SHA256_DIGEST_LENGTH],
+tx_out_t const *out);
 
 
 
