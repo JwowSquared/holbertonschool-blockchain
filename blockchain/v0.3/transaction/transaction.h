e2eebaa2be68ba6552d927a432aa1d8250670d81
JwowSquared <Jdwagner1@hotmail.com>
2022-04-05 04:40:26 +0000

    added task 4 source code

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -78,7 +78,9 @@ typedef struct unspent_tx_out_s
 
 
 
-tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+tx_out_t *tx_out_create(
+uint32_t amount,
+uint8_t const pub[EC_PUB_LEN]);
 
 unspent_tx_out_t *unspent_tx_out_create(
 uint8_t block_hash[SHA256_DIGEST_LENGTH],
@@ -87,8 +89,14 @@ tx_out_t const *out);
 
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
 
-uint8_t *transaction_hash(transaction_t const *transaction, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+uint8_t *transaction_hash(
+transaction_t const *transaction,
+uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 
-
+sig_t *tx_in_sign(
+tx_in_t *in,
+uint8_t const tx_id[SHA256_DIGEST_LENGTH],
+EC_KEY const *sender,
+llist_t *all_unspent);
 
 #endif /* _TRANSACTION_H_ */
