7429699a801e448071a68cb40505257852e66b2e
JwowSquared <Jdwagner1@hotmail.com>
2022-04-06 18:37:30 +0000

    added task 7 source code

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -117,4 +117,6 @@ int transaction_is_valid(
 	transaction_t const *transaction,
 	llist_t *all_unspent);
 
+transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index);
+
 #endif /* _TRANSACTION_H_ */
