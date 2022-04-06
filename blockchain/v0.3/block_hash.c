8e174f89af860e72d80ac1c2c2c9af691aee7608
JwowSquared <Jdwagner1@hotmail.com>
2022-04-06 20:40:42 +0000

    updated block_hash for task 12

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -10,7 +10,24 @@
 uint8_t *block_hash(block_t const *block,
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t hashable_size = sizeof(block->info) + block->data.len;
+	int tx_len = 0, target_len, i, offset;
+	transaction_t *t_token;
+	int8_t *hash_target;
 
-	return (sha256((int8_t const *)block, hashable_size, hash_buf));
+	offset = sizeof(block->info) + block->data.len;
+	if (block->transactions)
+		tx_len = llist_size(block->transactions);
+	target_len = offset + 32 * tx_len;
+	hash_target = malloc(target_len);
+	memcpy(hash_target, block, offset);
+	for (i = 0; i < tx_len; i++)
+	{
+		t_token = llist_get_node_at(block->transactions, i);
+		memcpy(hash_target + offset, t_token->id, 32);
+		offset += 32;
+	}
+
+	sha256(hash_target, target_len, hash_buf);
+	free(hash_target);
+	return (hash_buf);
 }
