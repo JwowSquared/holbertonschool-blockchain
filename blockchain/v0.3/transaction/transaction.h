17c8f689961909b7eac1ac01b23f08c43a089c14
JwowSquared <Jdwagner1@hotmail.com>
2022-04-06 06:58:58 +0000

    added task 5 source code

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -79,24 +79,38 @@ typedef struct unspent_tx_out_s
 
 
 tx_out_t *tx_out_create(
-uint32_t amount,
-uint8_t const pub[EC_PUB_LEN]);
+	uint32_t amount,
+	uint8_t const pub[EC_PUB_LEN]);
 
 unspent_tx_out_t *unspent_tx_out_create(
-uint8_t block_hash[SHA256_DIGEST_LENGTH],
-uint8_t tx_id[SHA256_DIGEST_LENGTH],
-tx_out_t const *out);
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH],
+	tx_out_t const *out);
 
-tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
+tx_in_t *tx_in_create(
+	unspent_tx_out_t const *unspent);
 
 uint8_t *transaction_hash(
-transaction_t const *transaction,
-uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+	transaction_t const *transaction,
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 
 sig_t *tx_in_sign(
-tx_in_t *in,
-uint8_t const tx_id[SHA256_DIGEST_LENGTH],
-EC_KEY const *sender,
-llist_t *all_unspent);
+	tx_in_t *in,
+	uint8_t const tx_id[SHA256_DIGEST_LENGTH],
+	EC_KEY const *sender,
+	llist_t *all_unspent);
+
+transaction_t *transaction_create(
+	EC_KEY const *sender,
+	EC_KEY const *receiver,
+	uint32_t amount,
+	llist_t *all_unspent);
+transaction_t *t_create(void);
+transaction_t *t_cleanup(transaction_t *out);
+uint32_t select_inputs(
+	llist_t *inputs,
+	llist_t *all_unspent,
+	uint32_t amount,
+	uint8_t key_in[EC_PUB_LEN]);
 
 #endif /* _TRANSACTION_H_ */
